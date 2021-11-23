d9933488eb1caeea786a3ab68efe334ed629d096
andrey-zubov <taac@mail.ru>
2021-11-23 21:08:37 +0300

    fill db

--- dentist/settings.py
+++ dentist/settings.py
@@ -78,7 +78,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [('my_redis', 6379)],
+            "hosts": [('127.0.0.1', 6379)],
         },
     },
 }
